09dd870bbe0aed0d30ca8d35707ed1a3f2ed25b4
Foysal <mohibullah2k0@gmail.com>
2025-05-10 19:10:24 +0600

    Implement project repositories and tasks routes: add resource routes for CompanyProjectRepositoryController and ProjectTaskController with appropriate naming conventions.

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TeamTypeController;
 use App\Http\Controllers\UserTypeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProjectTaskController;
 use App\Http\Controllers\ProjectsCategoryController;
+use App\Http\Controllers\CompanyProjectRepositoryController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -19,7 +22,7 @@ Route::get('/support', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])
-        // ->middleware('role:admin,superadmin')
+        ->middleware('role:admin,superadmin')
         ->name('dashboard');
 
     Route::get('manager-dashboard', [DashboardController::class, 'manager'])
@@ -42,6 +45,37 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('update-company-info', [UserController::class, 'companyInfo']);
     Route::post('update-company', [UserController::class, 'updateCompany']);
     Route::resource('teams', TeamController::class);
+
+    // repostories
+    Route::prefix('projects')->group(function () {
+        Route::resource('repositories', CompanyProjectRepositoryController::class)
+        ->except(['show'])
+        ->names([
+            'index' => 'projects.repositories.index',
+            'create' => 'projects.repositories.create',
+            'store' => 'projects.repositories.store',
+            'edit' => 'projects.repositories.edit',
+            'update' => 'projects.repositories.update',
+            'destroy' => 'projects.repositories.destroy',
+        ]);
+        // projects tasks
+        Route::resource('tasks', ProjectTaskController::class)
+            ->except(['show'])
+            ->names([
+            'index' => 'projects.tasks.index',
+            'create' => 'projects.tasks.create',
+            'store' => 'projects.tasks.store',
+            'edit' => 'projects.tasks.edit',
+            'update' => 'projects.tasks.update',
+            'destroy' => 'projects.tasks.destroy',
+        ]);
+
+        Route::put('projects/tasks/{id}/update-status', [ProjectTaskController::class, 'updateStatus'])->name('projects.tasks.updateStatus');
+    });
+    Route::resource('projects', ProjectController::class);
+    Route::put('projects/{id}/update-status', [ProjectController::class, 'updateStatus'])->name('projects.updateStatus');
+    Route::post('/projects/{project}/restore', [ProjectController::class, 'restore'])->name('projects.restore');
+    Route::delete('/projects/{project}/force-delete', [ProjectController::class, 'forceDelete'])->name('projects.forceDelete');
 
 });
 
